29aed7b6b25475d1a0d20dc20e101a032920d1da
Boluwatife <tolujunior7@gmail.com>
2026-03-24 16:50:29 +0100

    feat: add theme provider and toast notifications to _app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+      <Toaster richColors closeButton />
+    </ThemeProvider>
+  );
 }
